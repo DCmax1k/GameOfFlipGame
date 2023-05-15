2a9216da105970684186c43e705a5b7755c05af4
Dylan Caldwell <dylan.caldwell35@gmail.com>
2023-05-14 20:57:04 -0400

    cors

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/client/build'));
 app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
